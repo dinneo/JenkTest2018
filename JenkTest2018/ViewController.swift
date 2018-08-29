4534d170f4dc89115293448cd8d8939690662496
Neo Din <4465229+dinneo@users.noreply.github.com>
2018-08-29 19:18:51 +0800

    Initial Commit

---
+++ JenkTest2018/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JenkTest2018
+//
+//  Created by Neo Din on 2018/8/29.
+//  Copyright © 2018 Yi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
